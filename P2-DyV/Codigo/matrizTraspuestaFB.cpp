72900b12d1b69f67d01ac0813211a453255c3b9c
carlosmh2000 <56156574+carlosmh2000@users.noreply.github.com>
2020-03-30 17:41:23 +0200

    Update matrizTraspuestaFB.cpp

--- P2-DyV/Codigo/matrizTraspuestaFB.cpp
+++ P2-DyV/Codigo/matrizTraspuestaFB.cpp
@@ -8,14 +8,19 @@
 using namespace std;
 
 
-//Funcion que devuelve la traspuesta de una matriz dinamica dada
+/*
+Funcion que devuelve la traspuesta de una matriz dinamica dada
+Esta función realiza la trasposición simplemente colocando el elemento de la posición i,j 
+de la matriz original en la j,i de la matriz a devolver
+
+*/
 int **trasponer(int **m, int dim){
     //Reserva de espacio para la matriz traspuesta
     int **mt = new int*[dim];
     for(int i=0; i<dim; i++ ){
         mt[i] = new int [dim];
     }
-    //Bucles que trasponen la matriz dada
+    //Bucles que trasponen la matriz dada realizando el cambio de posiciones anteriormente citado
     for(int i=0; i<dim; i++){
         for(int j=0; j<dim; j++){
             mt[i][j]=m[j][i];
@@ -27,7 +32,8 @@ int **trasponer(int **m, int dim){
 int main(int argc, char ** argv){
 
     int util = atoi(argv[1]);
-
+    
+    //Comprobación de argumentos, ya que este programa solo admite un argumento, que será la dimensión de la matriz de entrada
     if(argc != 2){
         cout << "El programa solo recibe un parametro\n";
         return 1;
@@ -38,7 +44,7 @@ int main(int argc, char ** argv){
       cout << "Numero no es potencia de dos. Fin\n"<<endl;
       return 1;
     }
-    
+    int **traspuesta; // Declaracion del puntero a punteros que contendrá la traspuesta
     clock_t tantes, tdespues;
     //Reserva de espacio para la matriz dinamica
     int **matriz = new  int*[util];
@@ -66,31 +72,36 @@ int main(int argc, char ** argv){
   cout << endl << endl;
 */
 
-
+    //Medir el tiempo quee tarda en ejecutarse el método 
     tantes=clock();
-    matriz=trasponer(matriz, util);
+    traspuesta=trasponer(matriz, util);
     tdespues=clock();
-
+    
+    //Mostrar el resultado, con la dimensión 
     cout << util << "\t" << (double)(tdespues-tantes) / CLOCKS_PER_SEC << endl;
 
-    //Liberar memoria de la matriz
-
-
-/* for (int i = 0; i < util; i++)
-  {
-    for (int j = 0; j < util; j++)
+    /*
+    for (int i = 0; i < util; i++)
     {
-      cout << matriz[i][j] << " ";
-      if (j == util-1)
-        cout << endl;
+        for (int j = 0; j < util; j++)
+        {
+          cout << traspuesta[i][j] << " ";
+          if (j == util-1)
+            cout << endl;
+         }
     }
-  }
-  cout << endl << endl;
-*/
+    cout << endl << endl;
+    */
 
+    //Liberar memoria de las matrices
 
     for (int i = 0; i < util; i++)
         delete [] matriz[i];
 
     delete [] matriz;
+    
+    for (int i = 0; i < util; i++)
+        delete [] traspuesta[i];
+
+    delete [] traspuesta;
 }
