11b422edf30882302a2dcca4b65cd0f14a783c9d
José Mª Gómez García <33324337+gomezjosemaria@users.noreply.github.com>
2020-05-29 08:45:32 +0200

    ITV actualizado

--- P5-BacktrackingyBranchAndBound/Codigo/ITV/itv.cpp
+++ P5-BacktrackingyBranchAndBound/Codigo/ITV/itv.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <map>
 #include <climits>
+#include <ctime>
 
 using namespace std;
 
@@ -93,6 +94,11 @@ int main(int argc, char const *argv[]) {
   int size, aux_n, aux_t, lineas;
   vector<int> coches, coches_encola;
   vector< vector<int> > coches_listos;
+  clock_t tantes, tdespues;
+  long double tiempo_transcurrido;
+
+  srand(time(0));
+
   lineas = atoi(argv[2]);
 
   if (infile.is_open()) {
@@ -111,26 +117,90 @@ int main(int argc, char const *argv[]) {
   for (int i = 0; i < lineas; i++) {
     coches_listos.emplace_back();
   }
+  coches_listos.back().push_back(size - 1);
 
-  for (int i = 0; i < (int) coches.size(); i++) {
+  for (int i = 0; i < size - 1; i++) {
     coches_encola.push_back(i);
   }
 
+  /*
+
+  // IMPRESION DE DATOS PREPROCESADO
+
+  int tiempo_total;
+  tiempo_total = 0;
+
+  cout << "\n--- DATOS PREPROCESADO ---\n\n";
+
+  cout << "Coche:\tTiempo:\n";
+
+  for (int i = 0; i < (int) coches.size(); i++) {
+    cout << i + 1 << '\t' << coches[i] << '\n';
+    tiempo_total += coches[i];
+  }
+  cout << "Tiempo total: " << tiempo_total << '\n';
+  cout << '\n';
+
+  cout << "Coches en cola:\n";
+  for (int i = 0; i < (int) coches_encola.size(); i++) {
+    cout << coches_encola[i] + 1 << ' ';
+  }
+  cout << "\n\n";
+
+  cout << "Coches listos:\n";
+  for (int i = 0; i < (int) coches_listos.size(); i++) {
+    cout << "Linea " << i + 1 << ": ";
+    if (!coches_listos[i].empty()) {
+      for (int j = 0; j < (int) coches_listos[i].size(); j++) {
+        cout << coches_listos[i][j] + 1<< ' ';
+      }
+    }
+    else {
+      cout << "---";
+    }
+    cout << '\n';
+  }
+  cout << '\n';
+
+  // FIN IMPRESION PREPROCESADO
+
+  */
+
   ITV itv(coches, lineas);
+
+  tantes = clock();
+
   itv.resolver(coches_encola, coches_listos);
-  cout << size << '\n' << itv.get_t_min() << '\n';
+
+  tdespues = clock();
+
+  tiempo_transcurrido = (double)(tdespues-tantes) / CLOCKS_PER_SEC;
+
+  cout << size << '\t' << tiempo_transcurrido << '\n';
 
   coches_listos = itv.get_coches_listos();
 
+  /*
+
+  // IMPRESION SOLUCION
+
+  cout << "\n--- SOLUCION ---\n\n";
+
+  cout << "Tiempo solucion: " << itv.get_t_min() << "\n\n";
+
   for (int i = 0; i < lineas; i++) {
     aux_t = 0;
-    cout << "Linea " << i << '\n';
+    cout << "Linea " << i + 1 << ": ";
     for (int j = 0; j < (int) coches_listos[i].size(); j++) {
-      cout << coches_listos[i][j] << '\t' << coches[coches_listos[i][j]] << '\n';
+      cout << coches_listos[i][j] + 1 << ' ';
       aux_t += coches[coches_listos[i][j]];
     }
-    cout << "Tiempo en linea " << i << ": " << aux_t << '\n';
+    cout << "\nTiempo en linea " << i + 1 << ": " << aux_t << "\n\n";
   }
+
+  // FIN IMPRESION SOLUCION
+
+  */
 
   return 0;
 }
